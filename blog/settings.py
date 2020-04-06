3a28abdf60736b9e42e3f0a728bd3aa4b002ca1a
Jose Antonio Torres Garibay <jtorres@syscap.com.mx>
2020-04-05 23:16:33 -0500

    Change database to postgres

--- blog/settings.py
+++ blog/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_site',
+        'USER': 'postgres',
+        'PASSWORD': 'secret',
+        'HOST': 'localhost',
     }
 }
 
